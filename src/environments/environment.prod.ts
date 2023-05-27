2d4651681c5eb91bfa4cabb6d2618eae284f43af
Irams <herber.medina@gmail.com>
2023-05-27 08:15:18 -0600

    AngularAvd versión 1.2.0 desplegada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
-//TODO: Cambiar el URL de los servicios
 
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3000/api'
+  base_url: 'https://adminpro-backend-hims.herokuapp.com'
 };
